9b38a650145de6f08f75d75141eeb421132a9548
Edgar Avila <edgaravila01@yahoo.com>
2023-04-01 03:31:56 -0700

    phase 1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideheight){
   // Write your code here!
+  let greeting
+  if (rideheight <= 400) {
+    greeting = 'This one is on me!'
+    return greeting
+  }
+  else if (rideheight >= 400 && rideheight <= 2000) {
+    greeting = 'That will be twenty bucks.'
+    return greeting
+  }
+  else if (rideheight >= 2000 && rideheight <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+    return greeting
+  }
+  else if (rideheight > 2500) {
+    greeting = 'No can do.'
+    return greeting
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'?'Ok, sounds good.' : `No go.`
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
